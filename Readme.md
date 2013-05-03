701bc124b86a7a42f96d9d45e1fd47218d953155
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-05-04 00:21:27 +0200

    add sample app

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
-=Dart clickstart
+#Dart clickstart
+
+https://grandcentral.cloudbees.com/?CB_clickstart=https://raw.github.com/nfrancois/dart-clickstart/master/clickstart.json
---
+++ app/main.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'dart:json' as JSON;
+
+main() {
+  var port = int.parse(Platform.environment['PORT']);
+  HttpServer.bind('0.0.0.0', port).then((HttpServer server) {
+    print('Server started on port: ${port}');
+    server.listen((HttpRequest request) {
+      var resp = JSON.stringify({
+        'Dart on cloudbees': true,
+        'Environment': Platform.environment}
+      );
+      request.response..headers.set(HttpHeaders.CONTENT_TYPE, 'application/json')
+                      ..write(resp)
+                      ..close();
+    });
+  });
+}
